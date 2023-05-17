d8cabc1411e3c64a1f87e5388ecfc7a6c645913a
Vit Mistina <vit.mistina@gmail.com>
2023-05-17 08:53:35 +0200

    works in outlook now

--- content.js
+++ content.js
@@ -1,24 +1,52 @@
-const defaultTime = 10;
+const DEFAULT_TIME = 890;
+let countdown = DEFAULT_TIME;
 
-let t = defaultTime;
+const formatTime = (t) => {
+  const minutes = Math.floor(t / 60);
+  const seconds = t % 60;
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+};
 
-document.addEventListener("keydown", function (event) {
-  t = defaultTime;
+const restartCountdown = () => {
+  countdown = DEFAULT_TIME;
   chrome.runtime.sendMessage({
     action: "updateBadge",
-    badgeText: t.toString(),
+    badgeText: formatTime(countdown),
   });
+};
+
+let originalLocation = window.location.href;
+const observeHref = function (mutationsList, observer) {
+  if (window.location.href != originalLocation) {
+    originalLocation = window.location.href;
+    restartCountdown();
+  }
+};
+
+/* 
+    SCRIPT START
+*/
+restartCountdown();
+
+document.addEventListener("keydown", restartCountdown, true);
+
+const locationObserver = new MutationObserver(observeHref);
+locationObserver.observe(document.querySelector("head"), {
+  attributes: true,
+  childList: true,
 });
 
 setInterval(function () {
-  t = t - 1;
-  if (t === 0) {
+  countdown = countdown - 1;
+  chrome.runtime.sendMessage({
+    action: "updateBadge",
+    badgeText: formatTime(countdown),
+  });
+  if (countdown === 0) {
     window.location.reload();
     return;
   }
-  chrome.runtime.sendMessage({
-    action: "updateBadge",
-    badgeText: t.toString(),
-  });
-  console.log(`Time remaining ${t}`);
+  if (countdown < 0) {
+    restartCountdown();
+  }
 }, 1000);
