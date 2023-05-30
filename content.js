a573ca1003d0023f7b819d5c9d6b7311c7a70afe
Vit Mistina <vit.mistina@gmail.com>
2023-05-30 19:05:25 +0200

    0.1.3

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-const DEFAULT_TIME = 890;
+const DEFAULT_TIME = 10;
 let countdown = DEFAULT_TIME;
 
 const formatTime = (t) => {
@@ -15,6 +15,14 @@ const restartCountdown = () => {
   });
 };
 
+const restartCountdownForAlphabetKeys = (event) => {
+  const key = event.key;
+  const isAlphabetOrNumber = /^[a-zA-Z0-9ČĎŘŠŤŽčďřšťž]$/.test(key);
+  if (isAlphabetOrNumber) {
+    restartCountdown();
+  }
+};
+
 let originalLocation = window.location.href;
 const observeHref = function (mutationsList, observer) {
   if (window.location.href != originalLocation) {
@@ -23,12 +31,60 @@ const observeHref = function (mutationsList, observer) {
   }
 };
 
+const simulateActivity = () => {
+  // Get the current URL
+  // const url = new URL(window.location.href);
+
+  // // Check if the "t" query parameter is present
+  // if (url.searchParams.has("t")) {
+  //   // Change the value of the "t" query parameter to Date.now()
+  //   url.searchParams.set("t", Date.now());
+  // } else {
+  //   // Add the "t" query parameter with the value of Date.now()
+  //   url.searchParams.append("t", Date.now());
+  // }
+
+  // // Navigate to the new URL
+  // window.location.href = url.href;
+
+  // // Create a new keydown event
+  // const event = new KeyboardEvent("keydown", {
+  //   key: "*", // Specify the key as an asterisk
+  //   keyCode: 56, // Specify the key code for the asterisk key
+  //   which: 56, // Specify the which property for the asterisk key
+  // });
+
+  // Dispatch the event to the document
+  // document.dispatchEvent(event);
+  // document.querySelector("#O365_AppName").click();
+  document.querySelector("div[role='tablist'] #\\33").click();
+  restartCountdown();
+  setTimeout(
+    () => document.querySelector("div[role='tablist'] #\\31").click(),
+    200
+  );
+  // setTimeout(() => window.location.reload(), 400);
+
+  // calendarButton =
+  //   document.querySelector("#LeftRail button[aria-label='Calendar']") ||
+  //   document.querySelector("#LeftRail button[aria-label='Kalendář']");
+  // calendarButton.click();
+  // setTimeout(() => document.querySelector("#O365_AppName").click(), 1000);
+  // chrome.runtime.sendMessage({
+  //   action: "simulateActivity",
+  // });
+};
+
 /* 
     SCRIPT START
 */
+chrome.runtime.sendMessage({
+  action: "startTimer",
+});
+
 restartCountdown();
 
-document.addEventListener("keydown", restartCountdown, true);
+document.addEventListener("keydown", restartCountdownForAlphabetKeys, true);
 
 const locationObserver = new MutationObserver(observeHref);
 locationObserver.observe(document.querySelector("head"), {
@@ -43,8 +99,7 @@ setInterval(function () {
     badgeText: formatTime(countdown),
   });
   if (countdown === 0) {
-    window.location.reload();
-    return;
+    simulateActivity();
   }
   if (countdown < 0) {
     restartCountdown();
--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,22 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  let startTime = Date.now();
+  if (message.action === "startTimer") {
+    startTime = Date.now();
+  }
+  if (message.action === "simulateActivity") {
+    timeDifferenceInSeconds = (Date.now() - startTime) / 1000;
+    console.log(sender, message.action, timeDifferenceInSeconds);
+  }
   if (message.action === "updateBadge") {
     // Update the badge text
     chrome.action.setBadgeText({
       text: message.badgeText,
       tabId: sender.tab.id,
     });
+
+    if ([...message.badgeText].pop() === "0") {
+      timeDifferenceInSeconds = (Date.now() - startTime) / 1000;
+      console.log(sender, message.action, timeDifferenceInSeconds);
+    }
   }
 });
